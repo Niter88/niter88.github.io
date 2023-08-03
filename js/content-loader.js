1a248f77c372b66bfe3bba90a5ac75cc207912a3
Niter88 <niter88@gmail.com>
2023-08-02 22:49:05 -0300

    FIX ident

--- js/content-loader.js
+++ js/content-loader.js
@@ -3,53 +3,52 @@
 // Function to load the navigation bar content
 function loadNavigationBar() {
     $.ajax({
-      url: 'navigation-bar.html',
-      type: 'GET',
-      dataType: 'html',
-      success: function (data) {
-          $('#navigation-bar-container').replaceWith(data);
-      },
-      error: function () {
-        console.error('Error loading navigation bar content.');
-      }
+        url: 'navigation-bar.html',
+        type: 'GET',
+        dataType: 'html',
+        success: function (data) {
+            $('#navigation-bar-container').replaceWith(data);
+        },
+        error: function () {
+            console.error('Error loading navigation bar content.');
+        }
     });
-  }
-  
-  // Function to load the footer content
-  function loadFooter() {
+}
+
+// Function to load the footer content
+function loadFooter() {
     $.ajax({
-      url: 'footer-container.html',
-      type: 'GET',
-      dataType: 'html',
-      success: function (data) {
-          $('#footer-container').replaceWith(data);
-      },
-      error: function () {
-        console.error('Error loading footer content.');
-      }
+        url: 'footer-container.html',
+        type: 'GET',
+        dataType: 'html',
+        success: function (data) {
+            $('#footer-container').replaceWith(data);
+        },
+        error: function () {
+            console.error('Error loading footer content.');
+        }
     });
-  }
-  
-  // Function to load the main content
-  function loadMainContent() {
+}
+
+// Function to load the main content
+function loadMainContent() {
     // Use the constant variable defined in the index.html
     $.ajax({
-      url: MAIN_CONTENT_FILE,
-      type: 'GET',
-      dataType: 'html',
-      success: function (data) {
-          $('#main-content-container').replaceWith(data);
-      },
-      error: function () {
-        console.error('Error loading main content.');
-      }
+        url: MAIN_CONTENT_FILE,
+        type: 'GET',
+        dataType: 'html',
+        success: function (data) {
+            $('#main-content-container').replaceWith(data);
+        },
+        error: function () {
+            console.error('Error loading main content.');
+        }
     });
-  }
-  
-  // Call the functions to load the contents when the page loads
-  $(document).ready(function () {
+}
+
+// Call the functions to load the contents when the page loads
+$(document).ready(function () {
     loadNavigationBar();
     loadMainContent();
     loadFooter();
-  });
-  
+});
